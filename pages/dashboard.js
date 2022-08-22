62c7758bacf8363f8616b02861408d1f81caa8b2
STEPHEN ONOKWAKPOR <classicsteph11@gmail.com>
2022-08-22 18:27:59 +0100

    added stripe payment system

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -14,32 +14,43 @@ export default function Home() {
     return;
   }
 
+  if(!session.user.isSubscriber){
+    router.push('/subscribe');
+    return;
+  }
+
   return (
     <div className="dashboard">
       <section>
-        <h1 className="text-white text-3xl text-center">Projects</h1>
-        <div className="project-section flex justify-center space-x-20 pt-10 text-white">
-          <div className="project-1">
-            <h1 className="text-2xl font-bold  py-6">Project #1</h1>
-            <div className="project-1__todos">
-              <ol className="list-decimal text-xl">
+        <h1 className="text-white pt-20 text-3xl text-center">Projects</h1>
+
+        <div className="project-wrapper px-20 text-white md:grid md:grid-cols-4 my-20 ">
+
+        <div className="project-1 flex flex-col items-center">
+            <h1 className="text-2xl font-bold py-1  border-b border-gray-500">Project #1</h1>
+            <div className="project-1__todos py-4">
+              <ol className="list-disc text-xl">
                 <li>todo 1</li>
                 <li>todo 2</li>
                 <li>todo 3</li>
               </ol>
             </div>
           </div>
-          <div className="project-2">
-            <h1 className="text-2xl font-bold">Project #2</h1>
-            <div className="project-2__todos">
-              <ol className="list-decimal text-xl">
+
+          <div className="project-2 flex flex-col items-center">
+            <h1 className="text-2xl font-bold py-1 border-b border-gray-500">Project #2</h1>
+            <div className="project-2__todos py-4">
+              <ol className="list-disc text-xl">
                 <li>todo 1</li>
                 <li>todo 2</li>
                 <li>todo 3</li>
               </ol>
             </div>
           </div>
+
         </div>
+
+        
       </section>
     </div>
   );
